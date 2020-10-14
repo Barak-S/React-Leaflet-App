775db32353d38d9d8424e7147f727dcf41c17b41
Barak-S <barak.saidoff@gmail.com>
2020-10-14 11:16:15 -0400

    added fixed div with scollable content for parks

--- src/ParkContainer.js
+++ src/ParkContainer.js
@@ -9,20 +9,22 @@ class ParkContainer extends Component {
     render() {
         
         return (
-            <Card style={{ marginBottom: 22, padding: 12, minHeight: "25vh" }} className="ParkCol BoxShadow">
+            <Card style={{ marginBottom: 22, padding: 12, minHeight: "25vh" }} className="BoxShadow">
                 <Form inline style={{marginBottom: 8.5}}>
                 <Form.Control type="text" placeholder="Search Skate Spots!" className="mr-sm-2" />
                 <Button style={{backgroundColor: "#ED5145", borderColor: "#000000" , borderWidth: 0.25, borderRadius: 6, fontWeight: "600"}}>Search</Button>
                 </Form>
                 <h4 style={{textAlign: "center", fontWeight: "600"}}>Skate Spots Nearby</h4>
-                    {this.props.parks.map(park=>{
-                        return(
-                            <ParkCard
-                                key={park._id}
-                                park={park}
-                            />
-                        )
-                    })}
+                    <div className="ParkCol">
+                        {this.props.parks.map(park=>{
+                            return(
+                                <ParkCard
+                                    key={park._id}
+                                    park={park}
+                                />
+                            )
+                        })}
+                    </div>
             </Card>
         );
     }
