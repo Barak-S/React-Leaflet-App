a99d30c78ff386b16e20c714599193f3aa9317f7
Barak-S <barak.saidoff@gmail.com>
2020-10-17 12:34:02 -0400

    conditionally splice skate spot description

--- src/ParkCard.js
+++ src/ParkCard.js
@@ -23,7 +23,7 @@ class ParkCard extends Component {
                 <div style={{paddingLeft: 8}}>
                     <Card.Title style={{ textAlign: "center" }}>{this.props.park.name}</Card.Title>
                     { this.props.park.address && <h6>Address: {this.props.park.address}</h6>}
-                    <p style={{ color: "green", marginBottom: 5}}>{this.props.park.description.slice(0, 99)}...</p>
+                    <p style={{ color: "green", marginBottom: 5}}>{this.props.park.description.length > 100 ? this.props.park.description.slice(0, 99) + "..." : this.props.park.description }</p>
                     { this.props.park.features.length !== 0 &&
                     <div style={{paddingBottom: 8}}>
                         <p style={{ color: "#217AC9"}}>Spot Features:</p>
