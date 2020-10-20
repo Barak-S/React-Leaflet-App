8a3962f33e35274bce074630355e824300bb7afc
Barak-S <barak.saidoff@gmail.com>
2020-10-20 11:04:48 -0400

    link to portfolio in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Footer() {
     return (
         <div id="footer" style={{ textAlign: "center", fontSize: 17, fontWeight: "600", color: "#FFE485", backgroundColor: "#343A40" }}>
-            <p style={{margin: 0, padding: 7}}>© 2020, Barak Web Development</p>
+            <p style={{margin: 0, padding: 7}}>© 2020, <a alt="Barak Saidoff Developer Potfolio" target="_blank" href="https://baraksaidoff.com/" style={{color: "#FFE485" }}>Barak Web Development</a></p>
         </div>
     )
 }
